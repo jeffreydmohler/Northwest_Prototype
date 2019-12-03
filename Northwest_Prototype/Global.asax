eda872d4bcc446931a9af2d90fdb614c18b4ceac
Jeffrey Mohler <jeffreydmohler@outlook.com>
2019-12-02 17:07:25 -0700

    Add project files.

---
+++ Northwest_Prototype/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Northwest_Prototype.MvcApplication" Language="C#" %>
---
+++ Northwest_Prototype/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Northwest_Prototype.Startup))]
+namespace Northwest_Prototype
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
